deedd3fc491cf37db1d69d209e5a7ad7aa47921c
Sergey Azarenko <azarenko.s@gmail.com>
2021-12-17 09:21:17 +0200

    init

---
+++ vagcodecompare/vagcodecompare/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿
+namespace vagcodecompare
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.code1tb = new System.Windows.Forms.TextBox();
+            this.code2tb = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.result = new System.Windows.Forms.TextBox();
+            this.compare = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Code 1:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Code 2:";
+            // 
+            // code1tb
+            // 
+            this.code1tb.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.code1tb.Location = new System.Drawing.Point(75, 12);
+            this.code1tb.Name = "code1tb";
+            this.code1tb.Size = new System.Drawing.Size(695, 22);
+            this.code1tb.TabIndex = 2;
+            // 
+            // code2tb
+            // 
+            this.code2tb.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.code2tb.Location = new System.Drawing.Point(75, 40);
+            this.code2tb.Name = "code2tb";
+            this.code2tb.Size = new System.Drawing.Size(695, 22);
+            this.code2tb.TabIndex = 3;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.result);
+            this.groupBox1.Location = new System.Drawing.Point(12, 97);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(758, 461);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Difference";
+            // 
+            // result
+            // 
+            this.result.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.result.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.result.Location = new System.Drawing.Point(3, 18);
+            this.result.Multiline = true;
+            this.result.Name = "result";
+            this.result.ReadOnly = true;
+            this.result.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.result.Size = new System.Drawing.Size(752, 440);
+            this.result.TabIndex = 0;
+            this.result.WordWrap = false;
+            // 
+            // compare
+            // 
+            this.compare.Location = new System.Drawing.Point(15, 68);
+            this.compare.Name = "compare";
+            this.compare.Size = new System.Drawing.Size(158, 23);
+            this.compare.TabIndex = 5;
+            this.compare.Text = "Compare";
+            this.compare.UseVisualStyleBackColor = true;
+            this.compare.Click += new System.EventHandler(this.compare_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 570);
+            this.Controls.Add(this.compare);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.code2tb);
+            this.Controls.Add(this.code1tb);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Vag Code Compare";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox code1tb;
+        private System.Windows.Forms.TextBox code2tb;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox result;
+        private System.Windows.Forms.Button compare;
+    }
+}
+
---
+++ vagcodecompare/vagcodecompare/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace vagcodecompare
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void compare_Click(object sender, EventArgs e)
+        {
+            byte[] code1 = StringToByte(code1tb.Text);
+            byte[] code2 = StringToByte(code2tb.Text);
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < code1.Length; i++)
+            {
+                sb.AppendLine(string.Format("Byte [{0}] 01234567 {1}", i.ToString().PadLeft(2, '0'), code1[i] != code2[i] ? "diff" : string.Empty));
+                sb.AppendLine(string.Format("     [{0}] {1}", Convert.ToString(code1[i], 16).PadLeft(2, '0'), Reverse(Convert.ToString(code1[i], 2).PadLeft(8, '0'))));
+                sb.AppendLine(string.Format("     [{0}] {1}", Convert.ToString(code2[i], 16).PadLeft(2, '0'), Reverse(Convert.ToString(code2[i], 2).PadLeft(8, '0'))));
+                sb.AppendLine();
+            }
+
+            result.Text = sb.ToString();
+        }
+
+        private string Reverse(string input)
+        {
+            char[] inputarray = input.ToCharArray();
+            Array.Reverse(inputarray);
+            return new string(inputarray);
+        }
+
+        private byte[] StringToByte(string code)
+        {
+            byte[] buffer = new byte[code.Length / 2];
+
+            for (int i = 0; i < code.Length; i+=2)
+            {
+                buffer[i / 2] = byte.Parse(code.Substring(i, 2), System.Globalization.NumberStyles.HexNumber);
+            }
+
+            return buffer;
+        }
+    }
+}
